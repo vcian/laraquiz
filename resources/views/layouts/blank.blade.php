e420b0154dbb1727858aef8d04cabeb0aa018f7f
Devat Karetha <devat.karetha@viitorcloud.in>
2019-04-02 15:00:58 +0530

    Functional changes and added excel upload sample file in backend for upload a questions

--- resources/views/layouts/blank.blade.php
+++ resources/views/layouts/blank.blade.php
@@ -12,6 +12,7 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" rel="stylesheet" type="text/css">
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
